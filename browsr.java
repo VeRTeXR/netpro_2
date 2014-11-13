a1e17cd500c51becd600f24bfce7ff0539ecb1bc
VeRTeXR <p5vasut27665@gmail.com>
2014-11-13 13:50:13 +0700

    NP Assignment #2 dayo

---
+++ browsr.java
@@ -0,0 +1,141 @@
+package browserStuff;
+
+
+import com.teamdev.jxbrowser.chromium.BeforeSendHeadersParams;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserContext;
+import com.teamdev.jxbrowser.chromium.BrowserFactory;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.Font;
+
+
+public class browsr {
+
+	private JFrame frame;
+	private JTextField txtURL;
+	private JEditorPane editorPane,editorPane_1;
+	private JTextField txtURL_2;
+	private JTextArea txtrHeader;
+	private JTextArea txtrHea;
+	private JScrollPane scrollPane_3;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					browsr window = new browsr();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public browsr() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(169, 169, 169));
+		final Browser browser = BrowserFactory.create(); 
+		final Browser browser_2 = BrowserFactory.create();
+		
+		frame.setResizable(false);
+		frame.setTitle("Vasut Pongsattayapipat s5050453@kmitl.ac.th - Java Web Browser (Network Programming Class Assignment #2)");
+		frame.setBounds(100, 100, 1280, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		
+		JButton fwdButton = new JButton(">");
+		fwdButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				txtrHeader.setText("");
+				txtrHea.setText("");
+				threadDayo a = new threadDayo("Thread I",browser,editorPane,txtURL,txtrHea,txtrHea);
+				threadDayo b = new threadDayo("Thread II",browser_2,editorPane_1,txtURL_2,txtrHeader,txtrHea);
+				a.start();
+				//browser.loadURL(txtURL.getText());
+				b.start();
+				//browser_2.loadURL(txtURL_2.getText());
+			}
+		});
+		fwdButton.setBounds(1165, 12, 99, 20);
+		frame.getContentPane().add(fwdButton);
+		
+		txtURL = new JTextField("www.google.com");
+		txtURL.setBounds(10, 12, 617, 20);
+		frame.getContentPane().add(txtURL);
+		txtURL.setColumns(10);
+		
+		txtURL_2 = new JTextField("www.fakku.net");
+		txtURL_2.setBounds(647, 12, 508, 20);
+		frame.getContentPane().add(txtURL_2);
+		txtURL_2.setColumns(10);
+		
+		editorPane = new JEditorPane();
+		editorPane.setEditable(false);
+		//browser.loadURL(txtURL.getText());
+		//editorPane.add(browser.getView().getComponent());
+		//frame.add(editorPane);
+		
+		JScrollPane scrollPane = new JScrollPane(editorPane);
+		scrollPane.setBounds(10, 45, 617, 433);
+		frame.getContentPane().add(scrollPane);
+		
+		editorPane_1 = new JEditorPane();
+		editorPane_1.setEditable(false);
+		
+		//editorPane_1.add(browser_2.getView().getComponent());
+		
+		
+		JScrollPane scrollPane_2 = new JScrollPane(editorPane_1);
+		scrollPane_2.setBounds(647, 45, 617, 433);
+		frame.getContentPane().add(scrollPane_2);
+		
+		
+
+		txtrHea = new JTextArea();
+		txtrHea.setFont(new Font("Monospaced", Font.BOLD, 13));
+		txtrHea.setLineWrap(true);
+		txtrHea.setForeground(new Color(199, 21, 133));
+		txtrHea.setBackground(Color.GRAY);
+		txtrHea.setEditable(false);
+		txtrHea.setText("Header");
+		
+		JScrollPane scrollPane_1 = new JScrollPane(txtrHea);
+		scrollPane_1.setBounds(10, 489, 617, 162);
+		frame.getContentPane().add(scrollPane_1);
+		
+		txtrHeader = new JTextArea();
+		txtrHeader.setLineWrap(true);
+		txtrHeader.setFont(new Font("Monospaced", Font.BOLD, 13));
+		txtrHeader.setForeground(new Color(173, 255, 47));
+		txtrHeader.setBackground(Color.GRAY);
+		txtrHeader.setEditable(false);
+		txtrHeader.setText("Header");
+		
+		scrollPane_3 = new JScrollPane(txtrHeader);
+		scrollPane_3.setBounds(647, 489, 617, 162);
+		frame.getContentPane().add(scrollPane_3);
+		
+		//browser.loadURL(txtURL.getText());
+		//browser_2.loadURL(txtURL_2.getText());
+
+	}
+}
---
+++ threadDayo.java
@@ -0,0 +1,95 @@
+package browserStuff;
+
+import com.teamdev.jxbrowser.chromium.Browser;
+
+
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JEditorPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class threadDayo implements Runnable {
+	private Browser browser;
+	private JTextField txtURL;
+	private JTextArea txtrHeader;
+	private Thread thread;
+	private String url;
+	private String threadName;
+	private JTextArea tt;
+	private JEditorPane editorPane;
+
+	
+	
+	public threadDayo(String threadName,Browser browser,JEditorPane editorPane,JTextField txtURL,JTextArea txtrHeader,JTextArea tt){
+		this.threadName = threadName;
+		this.browser = browser;
+		this.editorPane = editorPane;
+		this.txtURL = txtURL;
+		this.txtrHeader = txtrHeader;
+		this.tt = tt;
+		url = "http://"+txtURL.getText();
+	}
+	
+	public void run() {
+		getLink();
+		getHeader();
+	}
+	
+	public void start ()
+	   {
+		  tt.append(threadName+" Creating Thread" +  "\n" );
+	      if (thread == null)
+	      {
+	         thread = new Thread (this, threadName);
+	         thread.start ();
+	         tt.append(thread.getName()+" Starting Thread" +  "\n" );
+	      }
+	   }
+	public void getLink() {
+		try{
+ 		 	thread.sleep(200);
+ 		 }
+ 		 catch(InterruptedException e){
+ 		 }
+		 
+		tt.append(thread.getName()+" Fetching Web" +"\n" );
+		try {
+			//browser.loadURL(url);
+			editorPane.setPage(url);
+		}
+		catch (Exception e) {
+			//.ignore
+		}
+		
+		 
+		
+	}
+	public void getHeader(){
+		tt.append(thread.getName()+" Get Header" +"\n" );
+		try{
+ 		 	thread.sleep(200);
+ 		 }
+ 		 catch(InterruptedException e){}
+		txtrHeader.append("________________________________________________________________\n");
+		 try {
+	            URLConnection conn = new URL(url).openConnection();
+	            Map<String, List<String>> map = conn.getHeaderFields();
+	            txtrHeader.append("Header of: "+url+"\n");
+
+	            for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+	            	txtrHeader.append(entry.getKey() + " : " + entry.getValue()+"\n");
+	            } 
+	 
+	        } catch (Exception e) {
+	        	txtrHeader.append("Could not load header from "+url);
+	        }	
+	}
+}
